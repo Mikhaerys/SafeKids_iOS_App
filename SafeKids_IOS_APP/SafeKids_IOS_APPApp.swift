a4684eef690d5dc180b52d6084e6510f8b9bd66d
Bryan Andres Suarez Sanchez <134110641+Bryan-Andres-Suarez-Sanchez@users.noreply.github.com>
2025-06-03 14:50:16 -0500

    Initial Files

---
+++ SafeKids_IOS_APP/SafeKids_IOS_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeKids_IOS_APPApp.swift
+//  SafeKids_IOS_APP
+//
+//  Created by Telematica on 3/06/25.
+//
+
+import SwiftUI
+
+@main
+struct SafeKids_IOS_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SafeKids_IOS_APPTests/SafeKids_IOS_APPTests.swift
@@ -0,0 +1,17 @@
+//
+//  SafeKids_IOS_APPTests.swift
+//  SafeKids_IOS_APPTests
+//
+//  Created by Telematica on 3/06/25.
+//
+
+import Testing
+@testable import SafeKids_IOS_APP
+
+struct SafeKids_IOS_APPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
